e1b142541aadf0fd5c375fa8397cc66e29406b49
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-12-06 15:05:17 -0500

    update

--- highlight_test.py
+++ highlight_test.py
@@ -4,9 +4,6 @@ import time
 
 pi_thing =  PiThing()
 
-switch = pi_thing.readSwitch()
-print('Switch:{0}'.format(switch))
-
 print('Blinking LED (Ctrl+C to stop)')
 while True:
     pi_thing=setLED(True)
