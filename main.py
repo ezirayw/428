a10e19942af7d881e37d958f4ba2458b1f571ca8
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-12-06 18:51:57 -0500

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pi_thing = PiThing()
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html',switch="foo")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True)
