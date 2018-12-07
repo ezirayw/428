bdafa921a6584e4ee9ecd8eb629162fe6ca915c1
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-12-06 19:20:50 -0500

    update

--- highlight_test.py
+++ highlight_test.py
@@ -6,7 +6,9 @@ pi_thing =  PiThing()
 
 print('Blinking LED (Ctrl+C to stop)')
 while True:
-    pi_thing.setLED(True)
+    pi_thing.setREDLED(True)
+    pi_thing.setUVLED(True)
     time.sleep(0.5)
-    pi_thing.setLED(False)
+    pi_thing.setREDLED(False)
+    pi_thing.setUVLED(False)
     time.sleep(0.5)
