ee4aec07452c3ceb373c1c2d025a4405a3fca28a
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-12-09 03:13:31 -0500

    update

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from highlight import PiThing
+import time
+import RPi.GPIO as GPIO
+
 
 app = Flask(__name__)
 pi_thing = PiThing()
 
+RED_PIN = 20
+UV_PIN = 16
+ECHO_PIN = 21
+TRIG_PIN = 26
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RED_PIN,GPIO.OUT) # LED output
+GPIO.setup(UV_PIN,GPIO.OUT)
+GPIO.setup(ECHO_PIN,GPIO.IN) # Echo input
+GPIO.setup(TRIG_PIN,GPIO.OUT) # Trigger output
 @app.route('/')
-def main():
-    return render_template('index.html',switch="foo")
+def index():
+    return render_template('index.html')
 
+@app.route('/index.html',methods = ['POST'])
+def hello():
+    skin = request.form["selectSkin"]
+    attachment = request.form["selectAttachment"]
+    color = request.form["selectWave"]
+    return redirect('/')
+if color == "Infrared":
+    pi_thing.setRedLED(True)
+if color == "UV":
+    pi_thing.setUVLED(True)
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000, debug=True)
